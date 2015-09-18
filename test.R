e1970f21144ff25f531d153286af70397783d2a9
Brett Taylor <brett.r.taylor@icloud.com>
2015-09-18 17:45:10 -0600

    First pass

---
+++ test.R
@@ -0,0 +1,16 @@
+pmltraining <- read.csv("./data/pml-training.csv",na.strings = c("","#DIV/0!"))
+obsCount <- nrow(pmltraining)
+tnames <- names(pmltraining)
+results <- c()
+index <- 1
+for(i in 1:ncol(pmltraining)){
+  numberOfNAs <-sum(is.na(pmltraining[,i]))
+  
+  if(numberOfNAs > (.5 * obsCount)) {
+    results[index] <- (tnames[i])
+    index <- index + 1
+  }
+}
+
+
+modelVars <- (pmltraining[,setdiff(tnames,results)])
